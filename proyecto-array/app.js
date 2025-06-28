2a80d81c808df640475370467bd5a8416dc51e4f
zoe.quiroz3 <zoequiroz03@gmail.com>
2025-06-28 12:29:00 -0300

    agregar participante y local storage

--- proyecto-array/app.js
+++ proyecto-array/app.js
@@ -1,27 +1,42 @@
 //crear el constructor
-function Participante(id,nombre,edad,email){
-    this.id=id;
-    this.nombre=nombre;
-    this.edad=edad;
-    this.email=email;
+function Participante(id, nombre, edad, email) {
+  this.id = id;
+  this.nombre = nombre;
+  this.edad = edad;
+  this.email = email;
 }
+
 let usuarios = [
   { id: 1, nombre: "Juan", edad: 25, email: "juan@mail.com" },
-  { id: 2, nombre: "Ana", edad: 30, email: "ana@mail.com" }
+  { id: 2, nombre: "Ana", edad: 30, email: "ana@mail.com" },
 ];
-function mostrar(){
-    //var=recuperar el id 
-    let id=document.querySelector("#hola");
-    //for q recorre el array
-    for (let i = 0; i < usuarios.length; i++) {
-      id.innerHTML=`nombre: ${usuarios[i].nombre}`;
-    }
-    //id.innerHTML= `nombre: ${usuarios.nombre[i], }`
+
+function agregarParticipant  const nombre = document.querySelector("#nombre").value;
+  const edad = document.querySelector("#edad").value;
+  const email = document.querySelector("#email").value;
+e() {
+  const id = usuarios[usuarios.length - 1].id + 1;
+
+  const participante = new Participante(id, nombre, edad, email);
+  usuarios.push(participante);
 }
-/*<div id=hola></*/
 
-window.onload = function() {
-    // Código que se ejecuta cuando la página termina de cargar: MOSTRAR APODO
-    alert("La página ha cargado completamente");
-}; 
+window.onload = () => {
+  // lo que haya dentro de localStorage se guarda en apodo
+  let apodo = localStorage.getItem("apodo");
+  
+  // Si no existe, le pedimos al usuario que lo ingrese
+  if (!apodo) {
+    let apodoIngresado;
 
+    while (apodoIngresado === null){
+      apodoIngresado = prompt("¡Hola! ¿Cómo te gustaría que te llame? Compartenos tu apodo:");
+    };
+
+    localStorage.setItem("apodo", apodoIngresado);
+    apodo = apodoIngresado
+  }
+  // Mostramos el mensaje en la pantalla
+  const mensaje = document.getElementById("mensajeAlCargar");
+  mensaje.textContent = `Bienvenido/a, ${apodo} 👋`;
+};
